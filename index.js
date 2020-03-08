e84eedf053048452cb62bbc21d20d5d7d343c142
Pieter-Willem Bekaert <pieterwillem.bekaert@gmail.com>
2020-03-08 15:58:22 +0100

    y

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.get('/',function(req,res){
     res.sendFile(__dirname+"/index.html");
 });
 
-app.listen(port, () => console.log("Example app listening on port!"));
+app.listen(port, () => console.log("Example app listening on port !"));
